762ddee72c117e0d85721f79b365858c1799c032
성은기 <tech@schaffengott.com>
2024-02-13 17:41:59 +0900

    초기 커밋.

---
+++ SampleToggleButton/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SampleToggleButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDeviceOn = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDevice = new System.Windows.Forms.PictureBox();
+            this.buttonOn = new System.Windows.Forms.Button();
+            this.buttonOff = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDeviceOn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDevice)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDeviceOn
+            // 
+            this.pictureBoxDeviceOn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBoxDeviceOn.Location = new System.Drawing.Point(28, 2);
+            this.pictureBoxDeviceOn.Name = "pictureBoxDeviceOn";
+            this.pictureBoxDeviceOn.Size = new System.Drawing.Size(26, 20);
+            this.pictureBoxDeviceOn.TabIndex = 84;
+            this.pictureBoxDeviceOn.TabStop = false;
+            this.pictureBoxDeviceOn.Click += new System.EventHandler(this.PictureBoxDevice_Click);
+            // 
+            // pictureBoxDevice
+            // 
+            this.pictureBoxDevice.BackColor = System.Drawing.Color.Black;
+            this.pictureBoxDevice.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxDevice.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxDevice.Name = "pictureBoxDevice";
+            this.pictureBoxDevice.Size = new System.Drawing.Size(56, 24);
+            this.pictureBoxDevice.TabIndex = 83;
+            this.pictureBoxDevice.TabStop = false;
+            this.pictureBoxDevice.Click += new System.EventHandler(this.PictureBoxDevice_Click);
+            // 
+            // buttonOn
+            // 
+            this.buttonOn.Location = new System.Drawing.Point(92, 21);
+            this.buttonOn.Name = "buttonOn";
+            this.buttonOn.Size = new System.Drawing.Size(75, 23);
+            this.buttonOn.TabIndex = 85;
+            this.buttonOn.Text = "On";
+            this.buttonOn.UseVisualStyleBackColor = true;
+            this.buttonOn.Click += new System.EventHandler(this.ButtonOn_Click);
+            // 
+            // buttonOff
+            // 
+            this.buttonOff.Location = new System.Drawing.Point(92, 50);
+            this.buttonOff.Name = "buttonOff";
+            this.buttonOff.Size = new System.Drawing.Size(75, 23);
+            this.buttonOff.TabIndex = 86;
+            this.buttonOff.Text = "Off";
+            this.buttonOff.UseVisualStyleBackColor = true;
+            this.buttonOff.Click += new System.EventHandler(this.ButtonOff_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.pictureBoxDeviceOn);
+            this.panel4.Controls.Add(this.pictureBoxDevice);
+            this.panel4.Location = new System.Drawing.Point(19, 37);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(56, 24);
+            this.panel4.TabIndex = 87;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(181, 96);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.buttonOff);
+            this.Controls.Add(this.buttonOn);
+            this.Name = "Form1";
+            this.Text = "sample";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDeviceOn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDevice)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDeviceOn;
+        private System.Windows.Forms.PictureBox pictureBoxDevice;
+        private System.Windows.Forms.Button buttonOn;
+        private System.Windows.Forms.Button buttonOff;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ SampleToggleButton/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleToggleButton
+{
+    public partial class Form1 : Form
+    {
+        private readonly object ctsLock = new object();
+        CancellationTokenSource cts;
+        private bool _buttonDeviceEnable = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBoxDevice_Click(object sender, EventArgs e)
+        {
+            SetDeviceAlarmToggle(!_buttonDeviceEnable);
+        }
+
+        private void ButtonOn_Click(object sender, EventArgs e)
+        {
+            if (_buttonDeviceEnable)
+                return;
+
+            SetDeviceAlarmToggle(true);
+        }
+
+        private void ButtonOff_Click(object sender, EventArgs e)
+        {
+            if (!_buttonDeviceEnable)
+                return;
+
+            SetDeviceAlarmToggle(false);
+        }
+
+
+        private async void SetDeviceAlarmToggle(bool enable)
+        {
+            _buttonDeviceEnable = enable;
+
+            if (enable)
+            {
+                CancellationToken token;
+                lock (ctsLock)
+                {
+                    if (cts != null)
+                    {
+                        if (!cts.IsCancellationRequested)
+                        {
+                            cts.Cancel();
+                        }
+                        cts.Dispose();
+                    }
+                    cts = new CancellationTokenSource();
+                    token = cts.Token;
+                }
+
+                bool result = await MoveComponentToPosition(pictureBoxDeviceOn, 28, 2, 2, token);
+
+                if (result)
+                {
+                    pictureBoxDeviceOn.Invoke((MethodInvoker)(() =>
+                    {
+                        pictureBoxDeviceOn.Location = new Point(28, 2);
+                        pictureBoxDeviceOn.Visible = false;
+                        pictureBoxDeviceOn.Visible = true;
+                    }));
+
+                    pictureBoxDevice.Invoke((MethodInvoker)(() =>
+                    {
+                        pictureBoxDevice.BackColor = Color.Black;
+                    }));
+                }
+            }
+            else
+            {
+                CancellationToken token;
+                lock (ctsLock)
+                {
+                    if (cts != null)
+                    {
+                        if (!cts.IsCancellationRequested)
+                        {
+                            cts.Cancel();
+                        }
+                        cts.Dispose();
+                    }
+                    cts = new CancellationTokenSource();
+                    token = cts.Token;
+                }
+
+                bool result = await MoveComponentToPosition(pictureBoxDeviceOn, 2, 2, 2, token);
+
+                if (result)
+                {
+                    pictureBoxDeviceOn.Invoke((MethodInvoker)(() =>
+                    {
+                        pictureBoxDeviceOn.Location = new Point(2, 2);
+                        pictureBoxDeviceOn.Visible = false;
+                        pictureBoxDeviceOn.Visible = true;
+                    }));
+
+                    pictureBoxDevice.Invoke((MethodInvoker)(() =>
+                    {
+                        pictureBoxDevice.BackColor = Color.DimGray;
+                    }));
+                }
+            }
+        }
+
+        // 토글 애니메이션
+        private async Task<bool> MoveComponentToPosition(Control component, int targetX, int targetY = 2, int speed = 2, CancellationToken cancellationToken = default)
+        {
+            bool isCompleted = false;
+
+            Point targetPosition = new Point(targetX, targetY);
+            bool moveLeft = component.Location.X > targetX;
+            //bool moveRight = component.Location.X < targetX;
+
+            while (component.Location != targetPosition)
+            {
+                try
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    int newX = moveLeft ? Math.Max(targetX, component.Location.X - speed) :
+                                         Math.Min(targetX, component.Location.X + speed);
+
+                    component.Invoke((MethodInvoker)(() =>
+                    {
+                        component.Location = new Point(newX, targetY);
+                    }));
+                    await Task.Delay(10, cancellationToken);
+
+                    if (component.Location == targetPosition)
+                    {
+                        isCompleted = true;
+                        break;
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    isCompleted = false;
+                    break;
+                }
+            }
+
+            if (isCompleted)
+            {
+                component.Invoke((MethodInvoker)(() =>
+                {
+                    component.Visible = false;
+                }));
+            }
+
+            return isCompleted;
+        }
+
+    }
+}
